ba8d2ca148dae485b512ec2cca8e35b2ffb36e2c
Vincent Cheval <vincent.cheval@icloud.com>
2013-12-14 15:01:24 +0000

    Release prepared

--- Source/length_equivalence/algorithm.ml
+++ Source/length_equivalence/algorithm.ml
@@ -1,3 +1,22 @@
+(*************************************************************************
+** APTE v0.4beta - Algorithm for Proving Trace Equivalence              **
+**                                                                      **
+** Copyright (C) 2013  Vincent Cheval                                   **
+**                                                                      **
+** This program is free software: you can redistribute it and/or modify **
+** it under the terms of the GNU General Public License as published by **
+** the Free Software Foundation, either version 3 of the License, or    **
+** any later version.                                                   **
+**                                                                      **
+** This program is distributed in the hope that it will be useful,      **
+** but WITHOUT ANY WARRANTY; without even the implied warranty of       **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 **
+** See the GNU General Public License for more details.                 **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program.  If not, see http://www.gnu.org/licenses/   **
+**************************************************************************)
+
 open Standard_library
 
 let test_for_length_on_leaves left_set right_set =
