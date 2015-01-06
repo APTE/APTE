b73bcd5d0af5f2a31ee5fc3b86d2ad9465c779ca
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-01-06 11:13:02 +0100

    typo

--- bench/launch_benchmark.py
+++ bench/launch_benchmark.py
@@ -67,7 +67,7 @@ def main():
                 list_binaries.append(bina)
             if "red_no_impro" in args.version:
                 bina = [i for i in list_binaries_tout if ("_1_" in i)][0]
-                bina = bina + " -with_por reduction improper"
+                bina = bina + " -with_por red improper"
                 list_binaries.append(bina)
         if args.difficulty:
             print(args.difficulty)
