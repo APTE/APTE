43c3af7f3baa990d16b7b9926d740fce57479a62
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-02-07 11:55:00 +0100

    more explanations about skl(P)=skl(Q)

--- Source/trace_equivalence/algorithm.ml
+++ Source/trace_equivalence/algorithm.ml
@@ -494,7 +494,9 @@ let apply_strategy_one_transition_por (* given .... *)
 
   (* ** FIRST step: labelises processes and update 'has_focus': at this point, some new processes coming from 
         breaking a parallel composition are in the multiset. All those new processes come from a unique parallel
-        composition, so we label them in an arbitrary order but consistently with right_symb_proc_list. *)
+        composition, so we label them in an arbitrary order but consistently with right_symb_proc_list.
+        Note that, by perfomring this step we also check skl(P)=skl(Q) except when P is a null process (represented
+        as an empty list in APTE). *)
 
     let proc_left_label, how_to_label = try_P proc_left proc_right (lazy (Process.labelise proc_left)) in
     let proc_right_label = try_P proc_left proc_right (lazy (Process.labelise_consistently how_to_label proc_right)) in
