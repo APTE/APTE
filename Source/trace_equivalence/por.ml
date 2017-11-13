f64b0a1042d43cdec74962859919e8c36f8a0100
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2017-11-13 10:49:03 +0100

    progress on translations

--- Source/trace_equivalence/por.ml
+++ Source/trace_equivalence/por.ml
@@ -1,31 +1,55 @@
 open Process_
 open Standard_library
-let pp = Printf.printf
-	   
-let importChannel ch = Channel.of_int (int_of_string (Term.display_term ch))
-(*  | _ -> pp "In generalized POR mode, channels should be constants." TODO *)
 
+let err s = Debug.internal_error s
+let pp s = Printf.printf s
+	   
+let importChannel = function
+  | Term.Func (s,[]) -> 
+     let strCh = Term.display_symbol_without_arity s in
+     let intCh = int_of_string (Term.display_symbol_without_arity s) in
+     Channel.of_int intCh
+  | _ -> err "In generalized POR mode, channels must be constants."
+	     
 let importVar x = Term_.var (Term.display_variable x)
 
-let importName n = Term_.var (Term.display_name n) (* fresh name here ? *)
+(* We suppose here that processes have been alpha-renamed in order
+   to avoid any clash variable-name or name-name. *)
+let importName n = Term_.var (Term.display_name n)
     
-let importPat = function
+let rec importPat = function
   | Process.Var x -> importVar x
-  | _ -> pp "In generalized POR mode, in let p = t in ..., x must be a variable"; exit 0
+  | Process.Tuple (s, tl) when Term.is_tuple s -> Term_.tuple (List.map importPat tl)
+  | _ -> err "In generalized POR mode, in let p = t in ..., p must be made of tuples and variables only."
 
+let importSymb s t1 = 		(* ugly workaround fo get a more compact function *)
+  if Term.is_equal_symbol Term.senc s then 2, Term_.senc t1
+  else if Term.is_equal_symbol Term.sdec s then 2, Term_.sdec t1
+  else if Term.is_equal_symbol Term.aenc s then 2, Term_.aenc t1
+  else if Term.is_equal_symbol Term.adec s then 2, Term_.adec t1
+  else if Term.is_equal_symbol Term.hash s then 1, Term_.hash
+  else if Term.is_equal_symbol Term.pk s then 1, Term_.pk
+  (* TODO: add those constructors in POR. *)
+  (* else if Term.is_equal_symbol Term.sk s then Term_.sk *)
+  (* else if Term.is_equal_symbol Term.sign s then Term_.sign *)
+  (* else if Term.is_equal_symbol Term.checksign s then Term_.checksign *)
+  else raise Not_found
+	     
 let rec importTerm = function
-  | Term.Func (symb,tl) ->
-     if Term.is_equal_symbol Term.senc symb
-     then let t1, t2 = List.hd tl, List.hd (List.tl tl) in
-	  Term_.senc (importTerm t1) (importTerm t2)
-     else if Term.is_equal_symbol Term.sdec symb
-     then let t1, t2 = List.hd tl, List.hd (List.tl tl) in
-	  Term_.sdec (importTerm t1) (importTerm t2)
-     (* other constructors *)
-     else if Term.is_tuple symb
-     then Term_.tuple (List.map importTerm tl)
-     else if tl = [] then Term_.ok ()       (* todo: generic constants *)
-     else exit 0			    (* TODO *)
+  | Term.Func (symb, tl) when Term.is_tuple symb ->
+     Term_.tuple (List,map importTerm tl)
+  | Term.Func (symb, []) -> ok () (* TODO: generic constants != ok *)
+  | Term.Func (symb, tl) ->
+     let t1 = List.hd tl in
+     let pt1 = importTerm t1 in
+     (try (match importSymb symb pt with
+	   | 1,f -> f pt1
+	   | 2,f ->  let t2 = List.hd tl, List.hd (List.tl tl) in
+		     let pt2 = importTerm t2 in
+		     f pt2)
+      with
+      | Not_found -> err "In generalized POR mode, allows constructors are senc, sdec, aenc, adec, hash, pk and tuples."
+      | _ -> err "[Internal error] Arity does not match.")
   | Term.Var x ->  importVar x
   | Term.Name n -> importName n
 
@@ -50,8 +74,9 @@ let importProcess proc =
     | Process.Out(t1,t2,proc,label) -> output (importChannel t1) (importTerm t2) (build proc)
     | Process.Let(pat,t,proc,label) -> if_eq (importPat pat) (importTerm t) (build proc) zero
     | Process.IfThenElse(formula,proc_then,proc_else,label) ->
-       match importFormula formula
-       with | (t1,t2) -> if_eq t1 t2 (build proc_then) (build proc_else)
+       match formula with
+       | Process.Eq (t1,t2) -> if_eq (importTerm t1) (importTerm t2) (build proc_then) (build proc_else)
+       | Process.Neq (t1,t2) -> if_neq (importTerm t1) (importTerm t2) (build proc_then) (build proc_else)
+       | _ -> "In generalized POR mode, tests in conditionals must be equality or disequality (no OR or AND)."
   in
   build proc
-	  
--- Source/trace_equivalence/por.mli
+++ Source/trace_equivalence/por.mli
@@ -1,4 +1,7 @@
 (** This module interfaces POR with Apte *)
 
-(** [import t] gives the LTS representation of a process [t] *)
+(** [importTerm t] gives the LTS representation of a term [t] *)
+val importTerm : Standard_library.Term.term -> Term_.t
+
+(** [importProcess p] gives the LTS representation of a process [p]*)
 val importProcess : Standard_library.Process.process -> Process_.t
